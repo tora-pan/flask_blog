328da7efd77c67370cb15c3536ceae6111008598
tora-pan <travis.pandos@gmail.com>
2024-04-11 07:19:47 -0700

    wip

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 
 def create_app():
@@ -12,3 +16,38 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     return app
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(25), unique=True, nullable=False)
+    email = Column(String(35), unique=True, nullable=False)
+    password = Column(String(80), nullable=False)
+    date_created = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+engine = create_engine('postgresql://travispandos:tatsulok1@localhost:5432/flask_blog')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# user = User(1234, 'tora_pan2', 'travis.pandos2@gmail.com', 'password1')
+# session.add(user)
+# session.commit()
+
+results = session.query(User).all()
+print([result.username for result in results])
--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        data = request.form
+    if request.method == 'GET':
+        return render_template("login.html")
 
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['GET', 'POST'])
 def logout():
-    return "<p>Logout</p>"
+    return redirect(url_for('auth.login'))
 
 
-@auth.route('/sign-up')
-def sign_up():
-    return "<p>Sign Up</p>"
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
